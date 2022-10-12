03769b0aa064a9515ca8bb6fc50a27769281faea
Webster <webster.jorgensen@gmail.com>
2022-10-12 16:49:08 -0500

    added subdomain config for prod server.js

--- backend/strapi/config/env/production/server.js
+++ backend/strapi/config/env/production/server.js
@@ -2,6 +2,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://api.lovestack.io',
   app: {
     keys: env.array('APP_KEYS'),
   },
