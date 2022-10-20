358d852c5e71906bd553b6e6f10e9caf11bc092f
Webster <webster.jorgensen@gmail.com>
2022-10-20 13:11:25 -0500

    deployment updates

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -3,10 +3,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'https://lovestack.io/strapi',
-  admin: {
-    url: '/strapi/admin',
-    serverAdminPanel: false
-  },
   app: {
     keys: env.array('APP_KEYS'),
   },
